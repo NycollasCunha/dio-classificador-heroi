94cf69cab37e10b34be834a44efb56c719ed2b64
NycollasCunha <nyteduardo@gmail.com>
2026-01-14 22:47:12 -0300

    Sistema de classificação concluído

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 let hero = "Tank";
+let rank = "";
+let xp = 5000;
+
+if (xp <= 1000) 
+{
+  rank = "Ferro";
+} 
+else if (xp <= 2000) 
+{
+  rank = "Bronze";
+} 
+else if (xp <= 5000) 
+{
+  rank = "Prata";
+} 
+else if (xp <= 7000) 
+{
+  rank = "Ouro";
+}
+else if (xp <= 8000) 
+{
+  rank = "Platina"
+}
+else if (xp <= 9000)
+{
+  rank = "Ascendente"
+}
+else if (xp <= 10000)
+{
+  rank = "Imortal"
+}
+else
+{
+  rank = "Radiante"
+}
+
+console.log("O herói " + hero + " está no nível " + rank + "!");
